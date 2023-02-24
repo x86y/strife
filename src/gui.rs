7261cf17571056633456a66fcf3a73945b67c666
x86y <me@x86y.dev>
2023-02-24 22:23:17 +0400

    functional, but not yet clean

--- src/gui.rs
+++ src/gui.rs
@@ -5,8 +5,8 @@ use eframe::egui::{self, scroll_area};
 use egui::FontFamily;
 use egui::{Color32, FontId, RichText};
 use std::env;
-use tokio::runtime::Handle;
-use tokio::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::Sender;
 use twilight_model::id::Id;
 
 #[derive(Debug, Default)]
@@ -27,75 +27,76 @@ pub async fn run() -> Result<(), eframe::Error> {
     };
     let cfg = Config::load().await.unwrap();
     let mut dc = Client::new(cfg.discord_key);
-    dc.current_channel = Some(Id::new(1051637259541159999));
-    let h = Handle::current();
+    dc.current_channel = Some(Id::new(1052777234454294569));
 
     eframe::run_native(
         env!("CARGO_PKG_NAME"),
         options,
         Box::new(move |cc| {
-            let (tx, rx) = tokio::sync::mpsc::channel(1);
             let (txp, mut rxp) = tokio::sync::mpsc::channel(1);
-            let app = Application::new(cc, txp, rx);
-            std::thread::spawn(move || {
-                h.block_on(async move {
-                    loop {
-                        if let Ok(send_val) = rxp.try_recv() {
-                            dc.rest
-                                .create_message(dc.current_channel.unwrap())
-                                .content(&send_val)
-                                .unwrap()
-                                .await
-                                .unwrap();
-                        };
+            let dc_msgs = Arc::new(Mutex::new(vec![]));
+            let dc_msgsp = dc_msgs.clone();
+            let arcctx = Arc::new(Mutex::new(cc.egui_ctx.clone()));
+            let app = Application::new(cc, dc_msgs, txp);
+            tokio::spawn(async move {
+                loop {
+                    if let Ok(send_val) = rxp.try_recv() {
+                        dc.rest
+                            .create_message(dc.current_channel.unwrap())
+                            .content(&send_val)
+                            .unwrap()
+                            .await
+                            .unwrap();
+                        arcctx.lock().unwrap().request_repaint();
+                    };
 
-                        let _ = dc.next_event().await;
-                        let Some(current_channel) = dc.current_channel else {
+                    let _ = dc.next_event().await;
+                    let Some(current_channel) = dc.current_channel else {
                             continue;
                         };
-                        let Some(message_ids) = dc
+                    let Some(message_ids) = dc
                             .cache
                             .channel_messages(current_channel) else {
                                 continue;
                         };
-                        let (items, _last_id) = message_ids.iter().rev().fold(
-                            (Vec::new(), None),
-                            |(mut items, mut last_id), message_id| {
-                                let message = dc.cache.message(*message_id).unwrap();
-                                let last = message.content().to_string();
-                                let author_id = message.author();
-                                let user = dc.cache.user(author_id).unwrap();
-                                let name = message
-                                    .member()
-                                    .unwrap()
-                                    .nick
-                                    .clone()
-                                    .unwrap_or_else(|| user.name.clone());
-                                let show_header = !last_id
-                                    .replace(author_id)
-                                    .map(|old_author_id| old_author_id == author_id)
-                                    .unwrap_or_default();
-                                let mut m = DcMessage {
-                                    username: name,
-                                    content: last,
-                                    ..Default::default()
-                                };
-                                if show_header {
-                                    m.is_header = true;
-                                    m.timestamp = display_timestamp(message.timestamp());
-                                }
-                                if let Some(timestamp) = message.edited_timestamp() {
-                                    let edited_timestamp = display_timestamp(timestamp);
-                                    m.edited = true;
-                                    m.timestamp = edited_timestamp;
-                                }
-                                items.push(m);
-                                (items, last_id)
-                            },
-                        );
-                        tx.send(items).await.unwrap();
-                    }
-                });
+                    let (items, _last_id) = message_ids.iter().rev().fold(
+                        (Vec::new(), None),
+                        |(mut items, mut last_id), message_id| {
+                            let message = dc.cache.message(*message_id).unwrap();
+                            let last = message.content().to_string();
+                            let author_id = message.author();
+                            let user = dc.cache.user(author_id).unwrap();
+                            let name = message
+                                .member()
+                                .unwrap()
+                                .nick
+                                .clone()
+                                .unwrap_or_else(|| user.name.clone());
+                            let show_header = !last_id
+                                .replace(author_id)
+                                .map(|old_author_id| old_author_id == author_id)
+                                .unwrap_or_default();
+                            let mut m = DcMessage {
+                                username: name,
+                                content: last,
+                                ..Default::default()
+                            };
+                            if show_header {
+                                m.is_header = true;
+                                m.timestamp = display_timestamp(message.timestamp());
+                            }
+                            if let Some(timestamp) = message.edited_timestamp() {
+                                let edited_timestamp = display_timestamp(timestamp);
+                                m.edited = true;
+                                m.timestamp = edited_timestamp;
+                            }
+                            items.push(m);
+                            (items, last_id)
+                        },
+                    );
+                    *dc_msgsp.lock().unwrap() = items;
+                    arcctx.lock().unwrap().request_repaint();
+                }
             });
             Box::new(app)
         }),
@@ -105,41 +106,39 @@ pub async fn run() -> Result<(), eframe::Error> {
 fn setup_fonts(ctx: &egui::Context) {
     let mut fonts = egui::FontDefinitions::default();
     fonts.font_data.insert(
-        "quicksand".to_owned(),
-        egui::FontData::from_static(include_bytes!("../assets/fonts/quicksand-regular.ttf")),
+        "cartograph".to_owned(),
+        egui::FontData::from_static(include_bytes!("../assets/fonts/cartograph.ttf")),
     );
     fonts
         .families
         .entry(egui::FontFamily::Proportional)
         .or_default()
-        .insert(0, "quicksand".to_owned());
+        .insert(0, "cartograph".to_owned());
     fonts
         .families
         .entry(egui::FontFamily::Monospace)
         .or_default()
-        .insert(0, "quicksand".to_owned());
+        .insert(0, "cartograph".to_owned());
     ctx.set_fonts(fonts);
 }
 
 struct Application {
     input_val: String,
-    messages: DcMessages,
+    messages: Arc<Mutex<DcMessages>>,
     txp: Sender<String>,
-    rx: Receiver<DcMessages>,
 }
 
 impl Application {
     fn new(
         cc: &eframe::CreationContext<'_>,
+        messages: Arc<Mutex<DcMessages>>,
         txp: Sender<String>,
-        rx: Receiver<DcMessages>,
     ) -> Self {
         setup_fonts(&cc.egui_ctx);
         Self {
             input_val: String::new(),
-            messages: vec![],
+            messages,
             txp,
-            rx,
         }
     }
 }
@@ -156,11 +155,7 @@ fn msg(ui: &mut egui::Ui, m: &DcMessage) {
         }
         ui.add_space(2.0);
         if m.edited {
-            ui.heading(format!(
-                "{} (last edited: {})",
-                m.content.clone(),
-                m.timestamp
-            ));
+            ui.heading(format!("{} (+ @{})", m.content.clone(), m.timestamp));
         } else {
             ui.heading(m.content.clone());
         }
@@ -178,12 +173,13 @@ fn input(ui: &mut egui::Ui, val: &mut String, txp: &mut Sender<String>) {
                 .margin(egui::Vec2 { x: 0.0, y: 8.0 }),
         );
         if multiline.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-            // hopeless ??
             let valc = val.clone();
             let txpc = txp.clone();
             tokio::spawn(async move {
                 txpc.send(valc).await.unwrap();
             });
+            val.clear();
+            multiline.request_focus();
         }
     });
 }
@@ -194,14 +190,12 @@ impl eframe::App for Application {
             fill: Color32::BLACK,
             ..Default::default()
         };
-        if let Ok(ms) = self.rx.try_recv() {
-            self.messages = ms;
-        }
-        if self.messages.is_empty() {
+        let msgs = self.messages.lock().unwrap();
+        if !msgs.is_empty() {
             egui::CentralPanel::default().frame(mf).show(ctx, |ui| {
                 ui.vertical_centered_justified(|ui| {
                     scroll_area::ScrollArea::vertical().show(ui, |ui| {
-                        self.messages.iter().for_each(|m| msg(ui, m));
+                        msgs.iter().for_each(|m| msg(ui, m));
                         ui.add_space(10.0);
                     });
                     input(ui, &mut self.input_val, &mut self.txp);
